1d6d5192d4b35e61aa66879ba17aeabd66a9627e
StevensCousinSabrina <167536167+StevensCousinSabrina@users.noreply.github.com>
2024-06-11 08:46:25 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -17,25 +17,32 @@ FPS = 60
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
-#define game variables
+# define game variables
 intro_count = 3
 last_count_update = pygame.time.get_ticks()
+score = [0, 0] #player scores, [p1, p2]
+match_over = False
+ROUND_OVER_COOLDOWN = 2000
 
-
-#define fighter variables
+# define fighter variables
 WARRIOR_SIZE = 162
 WARRIOR_SCALE = 7
-WARRIOR_OFFSET = [69,57]
+WARRIOR_OFFSET = [69, 57]
 WARRIOR_DATA = [WARRIOR_SIZE, WARRIOR_SCALE, WARRIOR_OFFSET]
 WIZARD_SIZE = 250
 WIZARD_SCALE = 6
 WIZARD_OFFSET = [112, 116]
 WIZARD_DATA = [WIZARD_SIZE, WIZARD_SCALE, WIZARD_OFFSET]
-MARTIAL2_SIZE = 162
-MARTIAL2_SCALE = 5
-MARTIAL2_OFFSET = [100, 100]
+MARTIAL2_SIZE = 200
+MARTIAL2_SCALE = 6
+MARTIAL2_OFFSET = [84, 78]
 MARTIAL2_DATA = [MARTIAL2_SIZE, MARTIAL2_SCALE, MARTIAL2_OFFSET]
+MARTIAL3_SIZE = 126
+MARTIAL3_SCALE = 6
+MARTIAL3_OFFSET = [44, 32]
+MARTIAL3_DATA = [MARTIAL3_SIZE, MARTIAL3_SCALE, MARTIAL3_OFFSET]
 
 # load background image
 bg_image = pygame.image.load("assets/images/backgrounds/plane.gif").convert_alpha()
@@ -51,6 +58,17 @@ WIZARD_ANIMATION_STEPS = [8, 8, 2, 8, 8, 3, 7]
 WARRIOR_ANIMATION_STEPS = [10, 8, 3, 7, 7, 3, 7]
 MARTIAL2_ANIMATION_STEPS = [4, 8, 2, 4, 4, 3, 7]
 MARTIAL3_ANIMATION_STEPS = [10, 8, 3, 7, 6, 3, 11]
+
+# font!
+count_font = pygame.font.Font("assets/Fonts/Turok.ttf", 80)
+count_font_border = pygame.font.Font("assets/Fonts/Turok.ttf", 90)
+score_font = pygame.font.Font("assets/Fonts/Turok.ttf", 30)
+
+
+# function for drawing text
+def draw_text(text, font, color, x, y):
+    img = font.render(text, True, color)
+    screen.blit(img, (x, y))
 
 
 # function for drawing background
@@ -70,9 +88,9 @@ def draw_health_bar(health, x, y):
 character1 = warrior_sheets
 character1_sheet = WARRIOR_ANIMATION_STEPS
 character1_data = WARRIOR_DATA
-character2 = martial2_sheets
-character2_sheet = MARTIAL2_ANIMATION_STEPS
-character2_data = MARTIAL2_DATA
+character2 = martial3_sheets
+character2_sheet = MARTIAL3_ANIMATION_STEPS
+character2_data = MARTIAL3_DATA
 
 # create two instances of fighters
 fighter_1 = Fighter(1, 200, 350, False, character1_data, character1, character1_sheet)
@@ -91,26 +109,28 @@ while run:
     draw_health_bar(fighter_2.health, 1011, 20)
 
     if intro_count <= 0:
-        #move fighters
+        # move fighters
         fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
         fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_1)
     else:
-        #update count timer
+        #draw count timer
+        draw_text(str(intro_count), count_font_border, BLACK, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
+        draw_text(str(intro_count), count_font, RED, SCREEN_WIDTH/2, SCREEN_HEIGHT/3)
+        # update count timer
         if pygame.time.get_ticks() - last_count_update >= 1000:
             intro_count -= 1
             last_count_update = pygame.time.get_ticks()
-        print(intro_count)
-
-
-
-
-    #update fighters
+    # update fighters
     fighter_1.update()
     fighter_2.update()
 
     # draw fighters
     fighter_1.draw(screen)
     fighter_2.draw(screen)
+
+    #check for player death
+    if round_over == False:
+        
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
